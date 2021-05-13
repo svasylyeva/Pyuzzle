257cca661024f9e78e8cd0fb9e3455cb06d7030a
Svitlana Vasylyeva <73995046+svasylyeva@users.noreply.github.com>
2021-05-13 22:08:35 +0100

    Initial commit
    - created file sudoku1000.csv with 1000 sudokus from kaggle dataset (link in code)
    - charles's  library (from class) was edited:
    unnecessary files were deleted,
    first fitness function for generated solution created

---
+++ Project/sudoku.py
@@ -0,0 +1,62 @@
+from charles.charles_sudoku import Individual
+from charles.selection import fps, tournament, rank
+from charles.mutation import swap_mutation
+from charles.crossover import cycle_co, pmx_co
+from random import choices
+
+from data.sudoku_data import quizz, Dim, solution
+from charles.selection import fps, tournament, rank
+from charles.mutation import binary_mutation
+from charles.crossover import single_point_co
+from random import random
+from operator import  attrgetter
+import csv
+
+
+def evaluate(self):
+     """A fitness function 
+
+    Returns:
+        int: sum
+    """
+     fitness = 0
+     # First attempt - DEPRECATED
+     # rows = [set() for _ in range(Dim)]
+     # columns = [set() for _ in range(Dim)]
+     # sections = [set() for _ in range(Dim)]
+     # for row in range(Dim):
+     #      for column in range(Dim):
+     #           value = self.representation[row * Dim + column]
+     #           rows[row].add(value)
+     #           columns[column].add(value)
+     #           sections[(row //3)*3 + (column//3)].add(value)
+     # fitness = sum(len(row) == Dim for row in rows) + sum(len(column) == Dim for column in columns) + sum(len(section) == Dim for section in sections)
+     
+     fitness = 0
+
+     # Fitness function for now only penalize violation of the rules 1-3
+     #  of rules ( rules of the game - link https://www.sudokuonline.io/tips/sudoku-rules )
+     # Adding some penalty scores for violation (assuming that it will ne minimization problem )
+     rows = [list() for _ in range(Dim)]
+     columns = [list() for _ in range(Dim)]
+     blocks = [list() for _ in range(Dim)]
+     for row in range(Dim):
+          for column in range(Dim):
+               value = self.representation[row * Dim + column]
+               rows[row].append(value)
+               columns[column].append(value)
+               blocks[(row //3)*3 + (column//3)].append(value)
+               
+     set_r = sum(abs(len(set(row)) - 9) for row in rows)
+     set_c = sum(abs(len(set(column)) - 9) for column in columns)
+     set_b = sum(abs(len(set(block)) - 9) for block in blocks)
+    
+     fitness = set_r + set_c + set_b
+     # Fitness function will be edited in the future
+     return fitness
+     
+     
+     return fitness
+
+# Monkey Patching
+Individual.evaluate = evaluate
